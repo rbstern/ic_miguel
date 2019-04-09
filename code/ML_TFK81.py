e555a162f6b3d54c58c516b79c1d975ad0170f82
rbstern <rbstern@gmail.com>
2019-04-09 12:23:42 -0300

    codigo adaptado em 20190409.

--- code/ML_TFK81.py
+++ code/ML_TFK81.py
@@ -1,39 +1,60 @@
 class Especie:
-  def __init__(self, meus_filhos, meu_valor):
-    self.filhos = meus_filhos
-    self.valor = meu_valor
+    def __init__(self, meu_pai, meu_valor):
+        self.filhos = []
+        self.pai = meu_pai
+        self.valor = meu_valor
+        if(meu_pai):
+            meu_pai.filhos.append(self)
 
-S1 = Especie(None, 1)
-S2 = Especie(None, 0)
-S3 = Especie(None, 0)
-S4 = Especie(None, 0)
-S5 = Especie(None, 1)
-S6 = Especie([S1, S2], None)
-S7 = Especie([S4, S5], None)
-S8 = Especie([S3, S7], None)
-S0 = Especie([S6, S8], None)
-
-folhas = [S1, S2, S3, S4, S5]
+def priori(a):
+    return 0.5
 
 def transicao(a, b):
-  if(a == b):
-    return 0.75
-  return 0.25
+    if(a == b):
+        return 0.75
+    return 0.25
 
 def L_condicional(esta_especie, valor_ancestral):
-  if(not(esta_especie.filhos)):
-    return transicao(esta_especie.valor, valor_ancestral)
-  else:
-    return (transicao(0, valor_ancestral)*
-L_condicional(esta_especie.filhos[0], 0)*
-L_condicional(esta_especie.filhos[1], 0))+(transicao(1, valor_ancestral)*
-L_condicional(esta_especie.filhos[0], 1)*
-L_condicional(esta_especie.filhos[1], 1))
+    if(not(esta_especie.filhos)):
+        return transicao(esta_especie.valor, valor_ancestral)
+    elif(not(esta_especie.pai)):
+        return(priori(0)*
+               L_condicional(esta_especie.filhos[0], 0)*
+                L_condicional(esta_especie.filhos[1], 0))+(priori(1)*
+                             L_condicional(esta_especie.filhos[0], 1)*
+                             L_condicional(esta_especie.filhos[1], 1))
+    else: 
+        return (transicao(0, valor_ancestral)*
+                L_condicional(esta_especie.filhos[0], 0)*
+                L_condicional(esta_especie.filhos[1], 0))+(transicao(1, valor_ancestral)*
+                             L_condicional(esta_especie.filhos[0], 1)*
+                             L_condicional(esta_especie.filhos[1], 1))
 
-testeS0=L_condicional(S0,1)
-testeS8=L_condicional(S8,1)
-testeS6=L_condicional(S6,1)
-testeS7=L_condicional(S7,1)
-testeS3=L_condicional(S3,1)
-print(testeS3)
+S0 = Especie(None, None)
+S6 = Especie(S0, None)
+S1 = Especie(S6, 1)
+S2 = Especie(S6, 0)
+S8 = Especie(S0, None)
+S3 = Especie(S8, 0)
+S7 = Especie(S8, None)
+S4 = Especie(S7, 0)
+S5 = Especie(S7, 1)
+likelihood = L_condicional(S0, None)
+print(likelihood)
 
+S0 = Especie(None, None)
+S6 = Especie(S0, None)
+S1 = Especie(S6, 1)
+S2 = Especie(S6, 0)
+S8 = Especie(S0, None)
+S5 = Especie(S8, 1)
+S7 = Especie(S8, None)
+S3 = Especie(S7, 0)
+S4 = Especie(S7, 0)
+likelihood = L_condicional(S0, None)
+print(likelihood)
+
+# Achar a arvore de maxima verossimilhança e
+# ler The base substitution probabilities
+# Bonus: adaptar o que fizemos para o caso em que
+# valor pode ser "A", "T", "C" ou "G".
