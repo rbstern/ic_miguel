9d8f99fb2fc23fffa31d1d6cf6bacca6033774e4
Luben Miguel Cruz Cabezas <luben@user-All-Series>
2019-10-02 11:22:17 -0300

    adicionado grafo do pulley principle

--- code/ML_TFK81.py
+++ code/ML_TFK81.py
@@ -57,6 +57,85 @@ S5 = Especie(S7, 1, meu_tempo = 0.3)
 S0.cria_L_condicional()
 S0.L_arvore
 
+class No:
+    def __init__(self, indice, vizinhos = None, atributos = None):
+        self.indice = indice
+        
+        if vizinhos is None:
+            self.dici_vizinhos = {}
+        else:
+            self.dici_vizinhos = vizinhos
+        
+        if atributos is None:
+            self.dici_atributos = {}
+        else:
+            self.dici_atributos = atributos
+            
+    def adiciona_vizinho(self, indice_viz, peso=1):
+        self.dici_vizinhos[indice_viz] = peso
+    
+    def retorna_atributos(self):
+        return self.dici_atributos
+    
+    def indices_vizinhos(self):
+        return list(self.dici_vizinhos.keys())
+    
+    def retorna_peso(self, indice_viz):
+        if indice_viz in self.dici_vizinhos:
+            return self.dici_vizinhos[indice_viz]
+        else:
+            print('No {} e {} não são vizinhos'.format(self.indice, indice_viz))
+
+
+class Grafo:
+    def __init__(self, raiz):
+        self.dici_nos = {}
+        self.transforma_arv(raiz)
+    
+    def transforma_arv(self,especie):
+        if (not(especie.pai)):  #eh raiz
+            for filho in especie.filhos:
+                no = No(filho, None, None)
+                self.dici_nos [filho] = no
+            self.dici_nos[especie.filhos[0]].adiciona_vizinho(especie.filhos[1],
+                                            especie.filhos[0].tempo + especie.filhos[1].tempo)
+            self.dici_nos[especie.filhos[1]].adiciona_vizinho(especie.filhos[0],
+                                            especie.filhos[0].tempo + especie.filhos[1].tempo)
+            self.transforma_arv(especie.filhos[0])
+            self.transforma_arv(especie.filhos[1])
+            
+        elif(not(especie.filhos)):   #eh folha
+            self.dici_nos[especie].dici_atributos["valor"] = especie.valor
+            
+        elif(especie.filhos is not None):     #eh interno
+            for filho in especie.filhos:
+                no = No(filho, None, None)
+                self.dici_nos[filho] = no
+            self.dici_nos[especie].adiciona_vizinho(especie.filhos[0],especie.filhos[0].tempo)
+            self.dici_nos[especie].adiciona_vizinho(especie.filhos[1], especie.filhos[1].tempo)
+            
+            if (especie.pai.pai is not None):
+                self.dici_nos[especie.filhos[0]].adiciona_vizinho(especie, especie.filhos[0].tempo)
+                self.dici_nos[especie.filhos[1]].adiciona_vizinho(especie, especie.filhos[1].tempo)
+                
+            self.transforma_arv(especie.filhos[0])
+            self.transforma_arv(especie.filhos[1])
+            
+    def no(self, indice_no):
+        return self.dici_nos[indice_no]
+    
+    def nos_grafo(self):
+        return list(self.dici_nos.values())
+                
+
+g = Grafo(S0)
+print(g.dici_nos[S6].retorna_peso(S8))
+print(g.dici_nos[S6].indices_vizinhos())
+
+
+
+
+
 # pulley principle: provar que vale sempre
 # começar leitura de finding maximum likelihood tree...
 # ir ate Finding Optimal Segment Lengths
